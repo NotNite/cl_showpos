292ed59ad68b1cb464909dfca8dc595e1dc49e13
Kaz Wolfe <root@kazwolfe.io>
2022-05-10 20:55:19 -0700

    Handle territories with multiple maps sanely
    - Adds a new method CurrentMapId that grabs the current map ID from AgentMap.
    - Uses this for Lumina lookups instead of territory

---
+++ cl_showpos/Game/MapHelper.cs
@@ -0,0 +1,9 @@
+﻿using FFXIVClientStructs.FFXIV.Client.UI.Agent;
+
+namespace cl_showpos.Game; 
+
+public static unsafe class MapHelper {
+    public static uint CurrentMapId() {
+        return AgentMap.Instance()->CurrentMapId;
+    }
+}
